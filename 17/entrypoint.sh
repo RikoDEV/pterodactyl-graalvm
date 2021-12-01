1db324e23a4321adc96e41e8271296bfa7b60c70
RikoDEV <18230443+RikoDEV@users.noreply.github.com>
2021-12-01 18:12:02 +0100

    Update entrypoint.sh

--- 17/entrypoint.sh
+++ 17/entrypoint.sh
@@ -1,12 +1,15 @@
 #!/bin/bash
 cd /home/container
 
-# Output Current Java Version
-java -version ## only really needed to show what version is being used. Should be changed for different applications
+# Print Java version
+java -version
+
+# Make internal Docker IP address available to processes.
+export INTERNAL_IP=`ip route get 1 | awk '{print $NF;exit}'`
 
 # Replace Startup Variables
 MODIFIED_STARTUP=`eval echo $(echo ${STARTUP} | sed -e 's/{{/${/g' -e 's/}}/}/g')`
 echo ":/home/container$ ${MODIFIED_STARTUP}"
 
 # Run the Server
-${MODIFIED_STARTUP}
+eval ${MODIFIED_STARTUP}
